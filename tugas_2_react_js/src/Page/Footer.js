c721747889e57f8a6c5ee94d596396e16ce943e1
fajrulfalah17 <fajruljunior45@gmail.com>
2021-09-05 22:45:53 +0700

    Tugas 2 React Js
    Tugas sudah dibuat

---
+++ tugas_2_react_js/src/Page/Footer.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return(
+            <div>
+                <h5>Tugas 2 React Js</h5>
+            </div>
+        );
+    };
+};
+
+export default Footer;
